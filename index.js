58ed8a61b5b923e17566c8c5e3a5d947fa0e7be6
John-Arboleda <johnjack0126@gmail.com>
2021-11-22 16:04:25 -0500

    Create function that adds a book to the displayed list

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ function adder(){
   const book = {};
   book.title = document.getElementById('title').value;
   book.author = document.getElementById('author').value;
-  return book
+  library.push(book);
 }
 
-adder()
+function addBook(bookObj) {
+  const bookList = document.getElementById('book-list')
+  const book = document.createElement("LI");
+  book.innerHTML = bookObj.title
+  bookList.appendChild(book)
+}
